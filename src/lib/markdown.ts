7bee4b4d5dd108d0c65d58451f0f83ab43842afa
Coy Catrett <63897775+coycatrett@users.noreply.github.com>
2025-03-07 13:38:51 +0000

    converted to vue since the website will not be as reactive as I thought.

--- src/lib/markdown.ts
+++
@@ -1,8 +0,0 @@
-import { remark } from 'remark';
-import html from 'remark-html';
-import katex  from 'remark-math';
-
-export async function markdownToHtml(markdown: string) {
-  const result = await remark().use(katex).use(html).process(markdown);
-  return result.toString();
-}
