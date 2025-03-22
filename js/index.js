9b751902001d8bbf6c3ed4e407c50b816ef0c81b
coycatrett <ctcatrett@gmail.com>
2025-03-22 08:46:30 -0400

    putting this project on hold until i finish my contributions to the nlab. i do not know enough time about web development to implement this project correctly

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
 /** Controls the webpage and its functionality **/
+
+const NUM_CHAPTERS = 4;
+const NUM_SECTIONS = { 0: 0, 1: 3, 2: 3, 3: 3 };
+
--- js/sols.js
+++ js/sols.js
@@ -1,18 +1,18 @@
 /** Controls and configures how markdown is handled withing the document **/
 async function loadMarkdown(filename) {
     const response = await fetch(filename);
-    const text = await response.text();
+    const solution = await response.text();
 
     // Convert Markdown to HTML
-    const html = marked.parse(text);
+    // const html = marked.parse(text);
 
     // Insert into the page
     // TODO: parse filename to obtain id or add new arguments
-    document.getElementById("solution-container").innerHTML = html;
+    document.getElementById("solution-container").innerHTML = solution;
 }
 
 // Example: Load a markdown file when page loads
-loadMarkdown("../sols/0/1/1.md");
+loadMarkdown("../sols/0/1/1.html");
 
 
 // solution only needs to load markdown if on that page
