5a01e5138406c7dff33f7d5530e28a9afd51dcc9
coycatrett <ctcatrett@gmail.com>
2025-09-26 19:56:58 -0400

    last working version

---
+++ data/toc_data.js
@@ -0,0 +1,334 @@
+
+const chapter_titles = [
+    'Chapter 0. Some Underlying Geometric Notions',
+    'Chapter 1. The Fundamental Group',
+    'Chapter 2. Homology',
+    'Chapter 3. Cohomology',
+    'Chapter 4. Homotopy Theory'
+]
+
+// description = 'Solutions to Allen Hatcher\'s Algebraic Topology - ${title}';
+
+export const toc = {
+    0: {
+        title: 'Chapter 0. Some Underlying Geometric Notions',
+        exercises: [1, 2],
+        extras: []
+    },
+    1: {
+        title: 'Chapter 1. The Fundamental Group',
+        sections: {
+            1: {
+                title: '1.1. Basic Constructions',
+                exercises: [1, 2],
+                extras: []
+            },
+            2: {
+                title: '1.2. Van Kampen\'s Theorem',
+                exercises: [],
+                extras: []
+            },
+            3: {
+                title: '1.3. Covering Spaces',
+                exercises: [],
+                extras: []
+            }
+        },
+        additional_topics: {
+            'A': {
+                title: '1.A. Graphs and Free Groups',
+                exercises: [],
+                extras: []
+            },
+            'B': {
+                title: '1.B. K(G,1) Spaces and Graphs of Groups',
+                exercises: [],
+                extras: []
+            }
+        },
+    },
+    2: {
+        title: 'Chapter 2. Homology',
+        sections: {
+            1: {
+                title: '2.1. Simplicial and Singular Homology',
+                exercises: [1, 2],
+                extras: []
+            },
+            2: {
+                title: '2.2. Computations and Applications',
+                exercises: [],
+                extras: []
+            },
+            3: {
+                title: '2.3. The Formal View Point',
+                exercises: [],
+                extras: []
+            }
+        },
+        additional_topics: {
+            'A': {
+                title: '2.A. Homology and Fundamental Group',
+                exercises: [],
+                extras: []
+            },
+            'B': {
+                title: '2.B. Classical Applications',
+                exercises: [],
+                extras: []
+            },
+            'C': {
+                title: '2.C. Simplicial Approximation',
+                exercises: [],
+                extras: []
+            }
+        },
+    },
+    3: {
+        title: 'Chapter 3. Cohomology',
+        sections: {
+            1: {
+                title: '3.1. Cohomology Groups',
+                exercises: [1, 2],
+                extras: []
+            },
+            2: {
+                title: '3.2. Cup Product',
+                exercises: [],
+                extras: []
+            },
+            3: {
+                title: '3.3. Poincare Duality',
+                exercises: [],
+                extras: []
+            }
+        },
+        additional_topics: {
+            'A': {
+                title: '3.A. Universal Coefficients for Homology',
+                exercises: [],
+                extras: []
+            },
+            'B': {
+                title: '3.B. The General Kunneth Formula',
+                exercises: [],
+                extras: []
+            },
+            'C': {
+                title: '3.C. H-Spaces and Hopf Algebras',
+                exercises: [],
+                extras: []
+            },
+            'D': {
+                title: '3.D. The Cohomology of SO(n)',
+                exercises: [],
+                extras: []
+            },
+            'E': {
+                title: '3.E. Bockstein Homomorphisms',
+                exercises: [],
+                extras: []
+            },
+            'F': {
+                title: '3.F. Limits and Ext',
+                exercises: [],
+                extras: []
+            },
+            'G': {
+                title: '3.G. Transfer Homomorphisms',
+                exercises: [],
+                extras: []
+            },
+            'H': {
+                title: '3.H. Local Coefficients',
+                exercises: [],
+                extras: []
+            }
+        },
+    },
+    4: {
+        title: 'Chapter 4. Homotopy Theory',
+        sections: {
+            1: {
+                title: '4.1. Homotopy Groups',
+                exercises: [1, 2],
+                extras: []
+            },
+            2: {
+                title: '4.2. Elementary',
+                exercises: [],
+                extras: []
+            },
+            3: {
+                title: '4.3. Connections with Cohomology',
+                exercises: [],
+                extras: []
+            }
+        },
+        additional_topics: {
+            'A': {
+                title: '4.A. Basepoints and Homotopy',
+                exercises: [],
+                extras: []
+            },
+            'B': {
+                title: '4.B. The Hopf INvariant',
+                exercises: [],
+                extras: []
+            },
+            'C': {
+                title: '4.C. H-Spaces and Hopf Algebras',
+                exercises: [],
+                extras: []
+            },
+            'D': {
+                title: '4.D. Cohomology of Fiber Bundles',
+                exercises: [],
+                extras: []
+            },
+            'E': {
+                title: '4.E. The Brown Representability Theorem',
+                exercises: [],
+                extras: []
+            },
+            'F': {
+                title: '4.F. Spectra and Homology Theories',
+                exercises: [],
+                extras: []
+            },
+            'G': {
+                title: '4.G. Gluing Constructions',
+                exercises: [],
+                extras: []
+            },
+            'H': {
+                title: '4.H. Eckmann-Hilton Duality',
+                exercises: [],
+                extras: []
+            },
+            'I': {
+                title: '4.I. Stable Splittings of Spaces',
+                exercises: [],
+                extras: []
+            },
+            'J': {
+                title: '4.J. The Loopspace of a Suspension',
+                exercises: [],
+                extras: []
+            },
+            'K': {
+                title: '4.K. The Don-Thom Theorem',
+                exercises: [],
+                extras: []
+            },
+            'L': {
+                title: '4.L. Steenrod Squares and Powers',
+                exercises: [],
+                extras: []
+            }
+        }
+    }
+}
+
+// TODO: make description function and keywords function
+
+export const chapters = [
+    {
+        title: chapter_titles[0],
+        num_exercises: 2,  // these are the only fields that are dynamic. could add it at the end...
+        num_additional_exercises: 0,
+        meta_data: {
+            title: chapter_titles[0],
+            description: 'Hatcher - Algebraic Topology Solutions: ' + chapter_titles[0],
+            keywords: ''
+        }
+    },
+    {
+        title: chapter_titles[1],
+        num_exercises: [
+            2,
+            0,
+            0,
+            0
+        ],
+        num_additional_exercises: 0,
+        meta_data: {
+            title: chapter_titles[1],
+            description: 'Hatcher - Algebraic Topology Solutions: ' + chapter_titles[1],
+            keywords: ''
+        }
+    },
+    {
+        title: chapter_titles[2],
+        num_exercises: [
+            0,
+            0,
+            0,
+            0
+        ],
+        num_additional_exercises: 0,
+        meta_data: {
+            title: chapter_titles[2],
+            description: 'Hatcher - Algebraic Topology Solutions: ' + chapter_titles[2],
+            keywords: ''
+        }
+    },
+    {
+        title: chapter_titles[3],
+        num_exercises: [
+            0,
+            0,
+            0,
+            0
+        ],
+        num_additional_exercises: 0,
+        meta_data: {
+            title: chapter_titles[3],
+            description: 'Hatcher - Algebraic Topology Solutions: ' + chapter_titles[3],
+            keywords: ''
+        }
+    },
+    {
+        title: chapter_titles[4],
+        num_exercises: [
+            0,
+            0,
+            0,
+            0
+        ],
+        num_additional_exercises: 0,
+        meta_data: {
+            title: chapter_titles[4],
+            description: 'Hatcher - Algebraic Topology Solutions: ' + chapter_titles[4],
+            keywords: ''
+        }
+    }
+]
+
+export const sections = [
+    [],
+    [
+        '1.1 Basic Constructions',
+        '1.2 Van Kampen\'s Theorem',
+        '1.3 Covering Spaces',
+        'Additional Topics'
+    ],
+    [
+        '2.1 Simplicial and Singular Homology',
+        '2.2 Computations and Applications',
+        '2.3 The Formal Viewpoint',
+        'Additional Topics'
+    ],
+    [
+        '3.1 Cohomology Groups',
+        '3.2 Cup Product',
+        '3.3 Poincare Duality',
+        'Additional Topics'
+    ],
+    [
+        '4.1 Homotopy Groups',
+        '4.2 Elementary Methods of Calculation',
+        '4.3 Connections with Cohomology',
+        'Additional Topics'
+    ]
+]
