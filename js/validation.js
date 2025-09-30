1225556a9ca49f10ed2e46b4f166ca38f666914e
coycatrett <ctcatrett@gmail.com>
2025-09-30 17:51:16 -0400

    implemented url validation and refactored location handler to remove switch case block

---
+++ js/validation.js
@@ -0,0 +1,28 @@
+import { toc } from '../data/toc_data.js';
+const routes = [
+    { // Home
+        regex: /^(\/home|\/)?$/,
+        validate_route: () => true
+    },
+    { // Chapter
+        regex: /^\/chapter-[0-4]$/,
+        validate_route: () => true
+    },
+    { // Exercise
+        regex: /^\/chapter-([0-4])(\/section-([1-3]))?\/exercise-(\d+)$/,
+        validate_route: ([, chapter, , section, exercise]) => {
+            const exercises = section
+                ? toc[chapter].sections[section].exercises
+                : toc[chapter].exercises;
+            return exercises.includes(Number(exercise));
+        }
+    }
+];
+
+export function validate(location) {
+    for (const { regex, validate_route } of routes) {
+        const match = regex.exec(location);
+        if (match) return validate_route(match);
+    }
+    return false;
+}
